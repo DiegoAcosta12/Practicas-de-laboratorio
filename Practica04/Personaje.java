a4c1acbb8176c09627ddd375bdda6fb46e81a939
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-09-02 08:32:16 -0500

    Practica04 8:30
    Borre un commit porque puse practica 05 en vez de 04

--- Practica04/Personaje.java
+++ Practica04/Personaje.java
@@ -2,6 +2,17 @@ public class Personaje{
 		private String nombre;
 	    private int edad;
 
+
+	    public Personaje(String nombre){
+	    	this.edad = 0
+	    }
+
+	    public int getEdad(String nombre);
+
+	    public boolean setEdad(Edad);
+	    if 
+
+
 	public void setNombre(String nombre){
 		this.nombre = nombre;
 	}
