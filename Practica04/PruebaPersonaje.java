e85db4f984c68dc7ca1cd75246af15fa29e6f804
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-09-02 07:58:48 -0500

    Practica04 8:00

--- Practica04/PruebaPersonaje.java
+++ Practica04/PruebaPersonaje.java
@@ -1,10 +1,19 @@
 public class PruebaPersonaje{
 	public static void main(String[]args){
-		Personaje alumno = new Personaje();
+		Personaje alumno = new Personaje("Kal El");
 		alumno.setNombre("Kobie Bryant");
 		alumno.saludar();
-		System.out.println("Modificando el nombre" + alumno.getNombre());
+		S.o.println(" Nombre: "+alumno.getNombre()+"Edad: " +almuno.getEdad());
+		S.o.println("Modificando el nombre"+alumno.getNombre());
 		alumno.setNombre("Michael Jordan");
-		alumno.saludar();
-	}
+		alumno.Saludar();
+		if(!alumno.setEdad(30)){
+			S.o.println("Edad sin cambios");
+		}
+		S.o.println("Nombre:"+alumno.getNombre()+"Edad:"+alumno.getEdad());
+		S.o.println("Modificando edad en 130");
+		if(!alumno.setEdad(130)){
+			S.o.println("Edad sin cambio");
+		}
+		S.o.println("Edad sin cambio");
 }
