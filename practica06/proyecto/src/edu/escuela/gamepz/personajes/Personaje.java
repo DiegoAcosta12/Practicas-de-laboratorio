6cb09214232170282f389685a46ffa0bb167d514
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-09-09 10:21:17 -0500

    Practica06 10:21
    Paso 9,10

--- practica06/proyecto/src/edu/escuela/gamepz/personajes/Personaje.java
+++ practica06/proyecto/src/edu/escuela/gamepz/personajes/Personaje.java
@@ -25,7 +25,7 @@ public class Personaje{
 	    	}
 
 	public void setNombre(String nombre){
-		if(nombre.length() >5 and nombre.length() <25)
+		if(nombre.length() > 5 && nombre.length() <25)
 		this.nombre = nombre;
 	}
 
--- practica06/proyecto/src/edu/escuela/gamepz/pruebas/PruebaPersonaje.java
+++ practica06/proyecto/src/edu/escuela/gamepz/pruebas/PruebaPersonaje.java
@@ -2,11 +2,11 @@ package edu.escuela.gamepz.pruebas;
 import edu.escuela.gamepz.personajes.*
 public class PruebaPersonaje{
 	public static void main(String[]args){
-		Personaje per01 = new personaje("Almendra",200)
-		Personaje per02 = new personaje("Ricardo",10)
-		Personaje per03 = new personaje("Bianca",1)
-		Personaje per04 = new persoanje("Leonel",-10)
-		Personaje per05 = new personaje("Hector",500)
+		Personaje per01 = new Personaje("Almendra",200)
+		Personaje per02 = new Personaje("Ricardo",10)
+		Personaje per03 = new Personaje("Bianca",1)
+		Personaje per04 = new Persoanje("Leonel",-10)
+		Personaje per05 = new Personaje("Hector",500)
 		System.out.println(per01.getDetalle());
 		System.out.println(per02.getDetalle());
 		System.out.println(per03.getDetalle());
@@ -20,49 +20,15 @@ public class PruebaPersonaje{
 		per06.setNombre("Francisco Xavier Zepeda Flores");
 		per08.setNombre("Dayan");
 		per10.setNombre("Axel Fabian Eduardo David");
-		
+		per07.setEdad((int)(Math.random()*100));
+		per09.setEdad((int)(Math.random()*100));
+		System.out.println("_ _ _");
+		System.out.println(per01.getDetalle());
+		System.out.println(per02.getDetalle());
+		Sysyem.out.println(per03.getDetalle());
+		System.out.println(per04.getDetalle());
+		System.out.println(per05.getDetalle());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		/*Personaje alumno = new Personaje("Kal El");
-		alumno.saludar();
-		System.out.println("Nombre:"+alumno.getNombre()+" Edad: " +alumno.getEdad());
-		System.out.println("Modificando el nombre"+alumno.getNombre());
-		alumno.setNombre("Michael Jordan");
-		alumno.saludar();
-		if(!alumno.setEdad(30)){
-			System.out.println("Edad sin cambios");
-		}
-
-
-		System.out.println("Nombre:"+alumno.getNombre()+" Edad: "+alumno.getEdad());
-		System.out.println("Modificando edad en 130");
-		if(!alumno.setEdad(130)){
-			System.out.println("Edad sin cambios");
-		}
-
-		System.out.println("Nombre:"+alumno.getNombre()+" Edad: "+alumno.getEdad
-
-	*/
+ 
 	}
 }
