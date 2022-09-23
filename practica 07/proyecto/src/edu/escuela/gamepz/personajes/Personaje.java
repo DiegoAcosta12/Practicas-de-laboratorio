1ffaf072a9a4455f27fbeb8fe773a567ad504401
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-09-23 07:42:01 -0500

    Practica07 7:40

---
+++ practica 07/proyecto/src/edu/escuela/gamepz/personajes/Personaje.java
@@ -0,0 +1,41 @@
+package edu.escuela.gamepz.personajes;
+public class Personaje{
+		private String nombre;
+	    private int edad;
+  
+  
+
+	    public Personaje(String nombre,){
+	    	this.nombre = nombre;
+	    }
+
+	    public int getEdad(){
+	    	return edad;
+	    }
+
+
+	    public boolean setEdad(int edad){
+	    	if(edad>0 && edad<120){
+	    	this.edad = edad;
+	    	return true;
+	     	}
+	    	return false;
+
+	    	}
+
+	public void setNombre(String nombre){
+		if(nombre.length() > 5 && nombre.length() <25)
+		this.nombre = nombre;
+	}
+
+	public String getNombre(){
+		return nombre;
+	}
+
+	public void saludar(){
+		System.out.println("Hola Alumno de POO "+nombre);
+	}
+	public String getDetalle(){
+		return nombre + "\t" +edad;
+	}
+}
