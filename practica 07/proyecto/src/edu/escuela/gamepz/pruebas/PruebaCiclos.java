7ae363aee43b26eb8b9ade0b8b3ec3994635e79a
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-09-23 08:00:10 -0500

    Practica07 8:00

---
+++ practica 07/proyecto/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
@@ -0,0 +1,8 @@
+public class PruebaCiclos{
+	public static void main(String[] args){
+		Personaje per01 = new personaje("Diego");
+		Personaje per02 = new personaje("Almendra");
+		Personaje per03 = new personaje("Bianca");
+		Personaje per04 = new personaje("Jordan");
+	}
+}
