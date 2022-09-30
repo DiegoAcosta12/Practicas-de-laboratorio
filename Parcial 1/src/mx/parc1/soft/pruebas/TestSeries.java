5d452f7a30f462ffdbaba62818790a8a20460f5f
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-09-30 08:21:02 -0500

    Parcial1 8:20

--- Parcial 1/src/mx/parc1/soft/pruebas/TestSeries.java
+++ Parcial 1/src/mx/parc1/soft/pruebas/TestSeries.java
@@ -2,16 +2,19 @@ package mx.parcial1.soft.series;
 import mx.parcial.soft.pruebas.*;
 public class TestSeries{
 	public static void main(String[] args) {
-		
+
 		Series s = new Serie();
 		int m = s.genNumero(100);
 		s.primos(m);
 		int o = s.primoN(m/10);
 		System.out.println(o);
-		int n = s.genNumero(100);
-		s.fibo(n);
-		int.fibo(n);
-		int p = s.fiboN(3,11);
-		System.out.println(p);
+
+		//int n = s.genNumero(100);
+		//s.fibo(n);
+
+		//int.fibo(n);
+		//int p = s.fiboN(3,11);
+
+		//System.out.println(p);
 	}
 }
--- Parcial 1/src/mx/parc1/soft/series/Series.java
+++ Parcial 1/src/mx/parc1/soft/series/Series.java
@@ -1,4 +1,4 @@
 package mx.parcial1.soft.series;
 public class Series{
-	
+	private int numero;
 }
