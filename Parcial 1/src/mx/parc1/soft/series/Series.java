988fb46bed36d5858da99b70ee8b250762b2a61b
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-09-30 09:01:03 -0500

    Parcial1 9:00

--- Parcial 1/src/mx/parc1/soft/series/Series.java
+++ Parcial 1/src/mx/parc1/soft/series/Series.java
@@ -1,9 +1,11 @@
 package mx.parcial1.soft.series;
-public class Series{
-	private int primoN;
 
-	public Series(int primoN){
-		this.primoN = PrimoN;
+public class Series{
+	private int numero;
+	public static boolean Series(int numero){
+	if(numero == 0)(numero == 1)(numero == 4){
+		
+
 	}
-	
+	}
 }
