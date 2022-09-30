a7a16833e307de250abd77dcaf4ee57e22208f4f
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-09-30 10:00:29 -0500

    Parcial1 10:00

--- Parcial 1/src/mx/parc1/soft/pruebas/TestSeries.java
+++ Parcial 1/src/mx/parc1/soft/pruebas/TestSeries.java
@@ -2,6 +2,7 @@ package mx.parc1.soft.pruebas;
 import mx.parc1.soft.Series.*;
 public class TestSeries{
 	public static void main(String[] args) {
+			 	
 		n = s.genNumero(10);
 		System.out.println(s.primoN(n));
 	}
