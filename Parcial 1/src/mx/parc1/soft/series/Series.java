f08740dea3b09b17169f6a1e30f32171054b56c9
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-09-30 09:10:35 -0500

    Parcial1 9:10

--- Parcial 1/src/mx/parc1/soft/series/Series.java
+++ Parcial 1/src/mx/parc1/soft/series/Series.java
@@ -1,11 +1,17 @@
 package mx.parcial1.soft.series;
 
-public class Series{
+public class esPar{
 	private int numero;
-	public static boolean Series(int numero){
-	if(numero == 0)(numero == 1)(numero == 4){
-		
 
+
+	public static boolean Series(int numero){
+		if(numero == 0, numero == 1, numero == 4){
+			return false;
 	}
+	for (int x = 2;x < numero / 2;x++ ) {
+		if (numero % x == 0)
+			return false;		
 	}
+	return true;
+	
 }
