0be99d919de2989583f59ae76b5e0843ee13601e
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-09-30 09:20:29 -0500

    Parcial1 9:20

--- Parcial 1/src/mx/parc1/soft/series/Series.java
+++ Parcial 1/src/mx/parc1/soft/series/Series.java
@@ -1,10 +1,9 @@
 package mx.parcial1.soft.series;
 
-public class esPar{
+public class Series{
+	//Algoritmo para saber si un numero es primo o no
 	private int numero;
-
-
-	public static boolean Series(int numero){
+	public static boolean esPar(int numero){
 		if(numero == 0, numero == 1, numero == 4){
 			return false;
 	}
@@ -13,5 +12,6 @@ public class esPar{
 			return false;		
 	}
 	return true;
-	
+
+
 }
