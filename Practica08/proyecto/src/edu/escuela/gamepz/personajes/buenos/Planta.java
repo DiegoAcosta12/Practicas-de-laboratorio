0c2548862a36991f7b08a9d3899050c84a0755b3
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-10-07 09:16:49 -0500

    Practica08 9:16

--- Practica08/proyecto/src/edu/escuela/gamepz/personajes/buenos/Planta.java
+++ Practica08/proyecto/src/edu/escuela/gamepz/personajes/buenos/Planta.java
@@ -3,21 +3,38 @@ public class Planta extends Personaje{
 	private char escudo;
 
 	public Planta(String nombre, int vida, char escudo){
-
+		super(nombre,vida);
+		this.escudo = escudo;
 	}
 	public Planta(String nombre, char escudo){
-
+		super(nombre, 3);
+		this.escudo = escudo;
 	}
 	public Planta(String nombre, int vida){
+		super(nombre,vida);
+		escudo = 'A';
 
 	}
 	public Planta(String nombre){
 
 	}
-	
+
 
 	public char getEscudo(){
+		return escudo;
 
 	}
 	
+	public void decVida(){
+
+	}
+	public void decVida(int d){
+
+	}
+	public void addVida(){
+
+	}
+	public void addVida(int d){
+
+	}
 }
--- Practica08/proyecto/src/edu/escuela/gamepz/personajes/malos/Zombie.java
+++ Practica08/proyecto/src/edu/escuela/gamepz/personajes/malos/Zombie.java
@@ -1,4 +1,12 @@
 package edu.escuela.gamepz.personajes.malos;
-public class Zombie{
-	
+public class Zombie extends Personaje{
+	private boolean ataque;
+
+	public Zombie
+
+
+
+	public boolean getAtaque{
+
+	}
 }
