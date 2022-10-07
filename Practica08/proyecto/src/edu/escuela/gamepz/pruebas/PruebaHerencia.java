9039f8dffea9f309b775b72c195f9dad190c64d5
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-10-07 11:18:08 -0500

    Practica08 11:18

--- Practica08/proyecto/src/edu/escuela/gamepz/pruebas/PruebaHerencia.java
+++ Practica08/proyecto/src/edu/escuela/gamepz/pruebas/PruebaHerencia.java
@@ -1,3 +1,33 @@
+package edu.escuela.gamepz.pruebas;
+import edu.escuela.gamepz.personajes.*;
 public class PruebaHerencia{
-	
-}
+	public static void main(String[] args) {
+		Personaje per01 = new Personaje ("David",100);
+		Personaje per02 = new Personaje ("Bianca");
+		Personaje plan01 = new Personaje ("Fabian",10,'B');
+		Personaje plan02 = new Personaje ("Almendra",50);
+		Personaje plan03 = new Personaje ("Ricardo",'C');
+		Personaje plan04 = new Personaje ("Silvia");
+		Personaje zom01 = new Personaje ("Armando",80,false);
+		Personaje zom02 = new Personaje ("Josseline",true);
+		Personaje zom03 = new Personaje ("Eduardo",100);
+
+		Personaje personajes [] = new Personaje[9];
+		personajes [1] = per01
+		personajes [2] = per02
+		personajes [3] = plan01
+		personajes [4] = plan02
+		personajes [5] = plan03
+		personajes [6] = plan04
+		personajes [7] = zom01
+		personajes [8] = zom02
+		personajes [9] = zom03
+
+		for ( : ) {
+			
+		}
+
+
+
+	}//llave main
+}//llave clase
