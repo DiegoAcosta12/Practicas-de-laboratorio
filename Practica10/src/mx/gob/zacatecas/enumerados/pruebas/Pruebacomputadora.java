31b703f5bc165464819d4dbe40acfecc21e1edc1
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-10-21 10:38:25 -0500

    Practica10 10:37
    Si acabe :)

--- Practica10/src/mx/gob/zacatecas/enumerados/pruebas/Pruebacomputadora.java
+++ Practica10/src/mx/gob/zacatecas/enumerados/pruebas/Pruebacomputadora.java
@@ -3,10 +3,10 @@ import mx.gob.zacatecas.enumerados.utilerias.*;
 import mx.gob.zacatecas.enumerados.equipos.*;
 public class Pruebacomputadora{
 	public static void main(String[] args) {
-		Computadora c1 = new Computadora("Lenovo",LAPTOP,DDR2);
-		Computadora c2 = new Computadora("HP",DESKTOP,DDR4);
-		Smallfactor s1 = new Smallfactor("Dell",WORKSTATION,DDR3,true);
-		Smallfactor s2 = new Smallfactor("Acer",SERVER,DDR,false);
+		Computadora c1 = new Computadora("Lenovo",Tipo.LAPTOP,Memoria.DDR2);
+		Computadora c2 = new Computadora("HP",Tipo.DESKTOP,Memoria.DDR4);
+		Smallfactor s1 = new Smallfactor("Dell",Tipo.LAPTOP,Memoria.DDR3,true);
+		Smallfactor s2 = new Smallfactor("Acer",Tipo.SERVER,Memoria.DDR,false);
 
 		Computadora computadoras[] = {c1,c2,s1,s2};
 		for (Computadora tmp : computadoras) {
