d1194146bba62c27413dada3c2fb1d951650cf83
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-11-04 09:15:52 -0600

    Practica12 9:15

--- Practica12/src/edu/escuela/gamepz/utils/Tablero.java
+++ Practica12/src/edu/escuela/gamepz/utils/Tablero.java
@@ -1,10 +1,21 @@
 package edu.escuela.gamepz.utils;
-import edu.escuela.gamepz.utils.PersException;
+import edu.escuela.gamepz.personajes.Personaje;
 public class Tablero{
 	public static int MAX_SIZE = 10;
 	Personaje[] personajes = new Personaje[MAX_SIZE];
+
 	private Tablero(int MAX_SIZE){
 		this.MAX_SIZE = MAX_SIZE;
 	} 
-
+	public static void Mostrar(){
+		System.out.println("\nInicio * * * Contenido del arreglo en el tablero\n");
+		for (Personaje tmp:personajes ) {
+			if(tmp = null ){
+				System.out.println();
+			}else{
+				System.out.println();
+			}
+			System.out.println("\nFin * * * Contenido del arreglo en Tablero\n");
+		}
+	}
 }
