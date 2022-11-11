3e3b5b01a816f2ee87dc536b7202d6773e0bd398
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-11-11 12:30:53 -0600

    Practica13 12:30

--- Practica13/src/edu/escuela/gamepz/pruebas/PruebaColeccion.java
+++ Practica13/src/edu/escuela/gamepz/pruebas/PruebaColeccion.java
@@ -2,6 +2,7 @@ package edu.escuela.gamepz.pruebas;
 import java.util.TreeSet;
 import java.util.LinkedList;
 import java.util.Collections;
+import java.util.ArrayList;
 import edu.escuela.gamepz.personajes.buenos.Planta;
 import edu.escuela.gamepz.personajes.malos.Zombie;
 import edu.escuela.gamepz.personajes.Personaje;
@@ -20,17 +21,35 @@ public class PruebaColeccion{
 			Zombie z4 = new Zombie("Armando",Tablero.genVida());
 
 			TreeSet<Personaje> ts = new TreeSet<>();
-			ts.add(p1);
-			ts.add(p2);
-			ts.add(p3);
-			ts.add(p4);
-			ts.add(z1);
-			ts.add(p5);
-			ts.add(z2);
-			ts.add(z3);
-			ts.add(z4);
-			for (Personaje datos: ts ){
-				System.out.println(datos);
+			//ts.add(p1);
+			//ts.add(p2);
+			//ts.add(p3);
+			//ts.add(p4);
+			//ts.add(z1);
+			//ts.add(p5);
+			//ts.add(z2);
+			//ts.add(z3);
+			//ts.add(z4);
+			for (Personaje tmp: datos){
+				System.out.println(tmp);
+			}
+			LinkedList<Personaje>linked = new LinkedList<>();
+			linked.addAll(ts);
+			System.out.println("---Orden Natural---");
+			for (Personaje p :ts ) {
+				System.out.println(p);
+			}
+			System.out.println("---Orden Vida---");
+			Collections.sort(lista,new ByVida());
+			for (Personaje p:lista ) {
+				System.out.println(p);
+			}
+			ArrayList arr = new ArrayList();
+			arr.addAll(lista);
+			System.out.println("---Orden Ternario---");
+			Collections.sort(arr,new BySize());
+			for (Personaje p:arr ) {
+				System.out.println(p);
 			}
 		}			
 	}
