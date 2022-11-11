3a45a3f61cf01cc61fa33e7b4f991852513dc0bb
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-11-11 11:49:12 -0600

    Practica13 11:49

--- Practica13/src/edu/escuela/gamepz/pruebas/PruebaColeccion.java
+++ Practica13/src/edu/escuela/gamepz/pruebas/PruebaColeccion.java
@@ -1,3 +1,31 @@
 public class PruebaColeccion{
+	public static void main(String[] args) {
+		Personaje[] datos = {
+			Planta p1 = new Planta("Fabian",Tablero.genVida(),MEDIO),
+			Planta p2 = new Planta("Bianca",Tablero.genVida()),
+			Planta p3 = new Planta("Fabian",Tablero.genVida(),),	
+			Planta p4 = new Planta("Armando",Tablero.genVida(),BAJO),	
+			Zombie z1 = new Zombie("Bianca",Tablero.genVida()),
+			Planta p5 = new Planta("Dayan",Tablero.genVida()),
+			Zombie z2 = new Zombie("Armando",Tablero.genVida(),false),
+			Zombie z3 = new Zombie("Dayan",Tablero.genVida(),true),
+			Zombie z4 = new Zombie("Armando",Tablero.genVida());
+
+			TreeSet<Personaje> ts = new TreeSet<>();
+			ts.add(p1);
+			ts.add(p2);
+			ts.add(p3);
+			ts.add(p4);
+			ts.add(z1);
+			ts.add(p5);
+			ts.add(z2);
+			ts.add(z3);
+			ts.add(z4);
+			for (Personaje datos: ts ) {
+			System.out.println(datos);
+		}
+		}
+
+	}
 	
 }
