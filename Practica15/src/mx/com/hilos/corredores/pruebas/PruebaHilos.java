133af58b436bb56ab3e1825ec36b37e623f612a6
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-11-25 09:38:32 -0600

    Practica15  9:38

--- Practica15/src/mx/com/hilos/corredores/pruebas/PruebaHilos.java
+++ Practica15/src/mx/com/hilos/corredores/pruebas/PruebaHilos.java
@@ -1,5 +1,6 @@
 package mx.com.hilos.corredores.pruebas;
 import mx.com.hilos.corredores.ventanas.Ventana;
+
 public class PruebaHilos{
 	public static void main(String[] args) {
 		Ventana v = new Ventana();
