34ad4a6d98606307afc2a0e7cdf1f4bd7e17dc4f
DiegoAcosta <diegoaacosta12345@gmail.com>
2022-11-25 10:49:45 -0600

    Practica15 10:49

--- Practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
+++ Practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -1,20 +1,27 @@
 package mx.com.hilos.corredores.hilos;
-
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 public class RunHilos{
 
 	public void Iniciar(JTextField txt1, JTextField txt2, JLabel elem1, JLabel elem2){
 		char ch1,ch2;
 		ch1 = txt1.getText().charAt(0);
 		ch2 = txt1.getText().charAt(0);
-		if()
+		if(ch1 = "0" || ch1 == " " ){
+			ch1 = "E"
+		}
+		if(ch2 = "0" || ch2 == " "){
+			ch2 = "M"
+		}
 
 
 
 
 
-		String salida1 salida2;
-		salida1 = txt1.getText().charAt(0);
-		salida2 = txt1.getText().charAt(0);
 		
+		String salida1 = " " + ch1;
+		String salida2 = " " + ch2;
+
+
 	}
 }
